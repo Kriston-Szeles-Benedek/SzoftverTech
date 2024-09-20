1130d35df533964cb7842d6f0a04932168785c39
Kriston-Széles Benedek <benmano03@gmail.com>
2024-09-20 19:26:53 +0200

    pisztoly kinda működik

--- scripts/Gun.cs
+++ scripts/Gun.cs
@@ -3,8 +3,8 @@ using System;
 
 public partial class Gun : Node2D
 {
-	[Export] PackedScene bullet;
-	[Export] float bullet_speed = 700f;
+	private PackedScene bullet;
+	[Export] float bullet_speed = 150f;
 	[Export] float bulletPerSec = 3f; // 3 lövés másodpercenként
 	[Export] float bulletDamage = 30f;
 	
@@ -17,6 +17,7 @@ public partial class Gun : Node2D
 	{
 		fireRate = 1f / bulletPerSec;  // fireRate = 1 / 3f = 0.33 másodpercenként
 		player = GetParent<WesternPlayer>();  // Feltételezzük, hogy a játékos a szülője
+		bullet = (PackedScene)ResourceLoader.Load("res://scene/bullet.tscn");
 	}
 
 	public override void _Process(double delta)
@@ -25,14 +26,13 @@ public partial class Gun : Node2D
 		timeUntilFire -= (float)delta;
 
 		// Lövés bemenet és időzítés ellenőrzése
-		if (Input.IsActionJustPressed("shoot") && timeUntilFire <= 0f)
+		if (Input.IsActionJustPressed("Shoot") && timeUntilFire <= 0f)
 		{
 			// Lövedék létrehozása
-			RigidBody2D bulletInstance = bullet.Instantiate<RigidBody2D>();
-			
+			RigidBody2D bulletInstance  = (RigidBody2D)bullet.Instantiate();
 			// Lövedék pozíciójának beállítása
 			bulletInstance.GlobalPosition = GlobalPosition;
-
+			AddChild(bulletInstance);
 			// Lövés iránya: a játékos mozgási iránya (Velocity) normálizálva
 			Vector2 shootDirection = player.Velocity.Normalized();
 			bulletInstance.LinearVelocity = shootDirection * bullet_speed;
@@ -43,5 +43,6 @@ public partial class Gun : Node2D
 			// Időzítés újraindítása
 			timeUntilFire = fireRate;
 		}
+		
 	}
 }
